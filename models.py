c67d1383aa20891879c9f516fe5dac5d689f949b
DIEME <89424921+DIEME-92@users.noreply.github.com>
2026-01-07 21:32:55 +0000

    Add files via upload

--- models.py
+++ models.py
@@ -8,7 +8,8 @@ class PredictionLog(Base):
     __tablename__ = "prediction_logs"
 
     id = Column(Integer, primary_key=True, index=True)
-    niveau = Column(String, nullable=False)
-    score = Column(Float, nullable=False)
-    profil = Column(String, nullable=False)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    niveau = Column(String)
+    profil = Column(String)
+    score = Column(Float)
+    # ❌ supprime created_at si tu ne veux pas l’utiliser
+
